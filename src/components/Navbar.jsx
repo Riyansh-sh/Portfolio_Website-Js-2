46cff0a6e564143ce2e1acf3ebe997e47556b605
Riyansh-sh <riyuu.2op@gmail.com>
2025-12-16 00:05:18 +0530

    Portfolio_website_1st-look
    this is my own macOs styled portfolio website but only its navbar.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { navLinks } from '../constants';
+const Navbar = () => {
+  return <nav>
+    <div> 
+        <img src="/images/logo.svg" alt="logo"/>
+        <p className='font-bold'> 
+            Riyansh's Portfolio
+        </p>
+        <ul> 
+            {navLinks.map(({id,name}) => (
+            <li key = {id}> 
+                <p> 
+                    {name}
+                </p>
+            </li>))}
+        </ul>
+    </div>
+  </nav>;
+};
+
+export default Navbar;
